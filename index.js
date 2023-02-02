a0b97d2b3c785e24b5c3d80c3408d463332f8785
Anton <buriak@edsson.com>
2023-02-02 18:51:35 +0200

    Setup

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Hi:)')
 })
+
+app.get('/feedback', (req, res) => {
+    const mailgun = require("mailgun-js");
+    const mg = mailgun({apiKey: process.env.apiKey, domain: process.env.domain});
+    const data = {
+        from: 'Bookmanize Feedback <me@samples.mailgun.org>',
+        to: 'bookmanize@gmail.com',
+        subject: 'New Feedback '+ new Date().toString(),
+        text: `Message: ${req.query.msg}; Selected variant: ${req.query.variant}`
+    };
+    mg.messages().send(data, function (error, body) {
+        console.log(error,body);
+        res.send()
+    });
+})
+
 app.listen(process.env.PORT || 3000)
