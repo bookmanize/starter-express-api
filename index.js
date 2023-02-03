8953c45ac11c60f37fefc6cc349a3a3a278b8cc5
Anton <anton@go.com>
2023-02-03 16:05:11 +0200

    Use another mailgun lib

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-
+const Mailgun = require("mailgun.js");
+const formData = require('form-data');
 const app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 app.use(cors({
     origin: '*'
 }))
 
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({ username: 'api', key: process.env.apiKey, timeout: 60000 });
+
 app.get('/', (req, res) => {
-    const mailgun = require("mailgun-js");
-    const mg = mailgun({apiKey: process.env.apiKey, domain: process.env.domain});
+    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log('Request: ', fullUrl, req.query); // TODO remove it. @Anton
+    if(!req.query.msg && !req.query.variant){
+        console.error('No data provided');
+        res.send('Error');
+        return;
+    }
+
     const text = `Message: ${req.query.msg}; Selected variant: ${req.query.variant}`;
     const data = {
         from: 'Bookmanize Feedback <me@samples.mailgun.org>',
@@ -17,14 +31,16 @@ app.get('/', (req, res) => {
         subject: `New Feedback ${new Date().toString()}`,
         text
     };
-
     console.log('Data: ',data);
 
-    mg.messages().send(data, function (error, body) {
-        console.log('Response: ',body);
-        error && console.error(error)
+
+    mg.messages.create(process.env.domain, data).then((mgRes)=>{
+        console.log('Response: ',mgRes);
         res.send('Done')
+    }).catch((error)=>{
+        console.error(error)
     });
+
 })
 
 app.listen(process.env.PORT || 3000)
