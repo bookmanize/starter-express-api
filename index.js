dac4432b365fe1dbc4c63fc0858bca089d8dd46f
Anton <anton@go.com>
2023-02-03 15:41:36 +0200

    Fix cors issue, improve logs

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+    origin: '*'
+}))
+
 app.get('/', (req, res) => {
     const mailgun = require("mailgun-js");
     const mg = mailgun({apiKey: process.env.apiKey, domain: process.env.domain});
+    const text = `Message: ${req.query.msg}; Selected variant: ${req.query.variant}`;
     const data = {
         from: 'Bookmanize Feedback <me@samples.mailgun.org>',
         to: 'bookmanize@gmail.com',
-        subject: 'New Feedback '+ new Date().toString(),
-        text: `Message: ${req.query.msg}; Selected variant: ${req.query.variant}`
+        subject: `New Feedback ${new Date().toString()}`,
+        text
     };
+
+    console.log('Data: ',data);
+
     mg.messages().send(data, function (error, body) {
-        console.log(error,body);
+        console.log('Response: ',body);
+        error && console.error(error)
         res.send('Done')
     });
 })
